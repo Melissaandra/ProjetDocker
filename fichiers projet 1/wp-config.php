1c415cf3da739aaa8e363be41d3bf8d524eb6506
Melissaandra <151726521+Melissaandra@users.noreply.github.com>
2023-12-21 13:05:31 +0100

    Projet1
    tous les fichiers utilisés pour le projet 1

---
+++ fichiers projet 1/wp-config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME', 'db');
+define('DB_USER', 'toto');
+define('DB_PASSWORD', 'toto');
+define('DB_HOST', 'mariadb'); //  nom du service défini dans docker-compose.yml
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
+
